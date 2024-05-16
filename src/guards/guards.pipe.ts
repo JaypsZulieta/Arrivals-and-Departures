ba5ef5fccafb9dc3f6112bd98e786dfab42ebe79
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-17 06:29:05 +0800

    fix(GuardsPipe): Add helper methods to the class to make it more readable

--- src/guards/guards.pipe.ts
+++ src/guards/guards.pipe.ts
@@ -16,7 +16,7 @@ export class GuardsPipe implements PipeTransform<any, Promise<Guard>> {
       const email = await objectValue.pickString('email');
       const password = await objectValue.pickString('password');
 
-      if (sex.toLowerCase() != 'male' && sex.toLowerCase() != 'female')
+      if (this.notValidSex(sex))
         throw new BadRequestException('sex must either be Male or Female');
 
       const guard = new GuardBuilder()
@@ -26,15 +26,21 @@ export class GuardsPipe implements PipeTransform<any, Promise<Guard>> {
         .email(email)
         .password(password)
         .build();
-
-      if (sex.toLowerCase() == 'male') guard.setSex(Sex.MALE);
-      else guard.setSex(Sex.FEMALE);
-
-      return guard;
+      return this.setSex(sex, guard);
     } catch (error) {
       if (error instanceof ValidationError)
         throw new BadRequestException(error.message);
       throw error;
     }
+  }
+
+  private notValidSex(sex: string): boolean {
+    return sex.toLowerCase() != 'male' && sex.toLowerCase() != 'female';
+  }
+
+  private setSex(sex: string, guard: Guard): Guard {
+    if (sex.toLowerCase() == 'male') guard.setSex(Sex.MALE);
+    else guard.setSex(Sex.FEMALE);
+    return guard;
   }
 }
