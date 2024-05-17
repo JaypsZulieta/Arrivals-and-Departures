669cb3bf7585241836f60a31bbbc4c11de861e08
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-17 20:39:25 +0800

    test (GuardsPipe): Add initial test for the GuardsPipe class

--- src/guards/guards.pipe.spec.ts
+++ src/guards/guards.pipe.spec.ts
@@ -1,7 +1,25 @@
+import { Guard } from './guards.entity';
 import { GuardsPipe } from './guards.pipe';
 
 describe('GuardsPipe', () => {
   it('should be defined', () => {
     expect(new GuardsPipe()).toBeDefined();
+  });
+
+  describe('transform', () => {
+    it('should return an instance of Guard if the input data is acceptable', () => {
+      const data = {
+        firstname: 'Jaypee',
+        middlename: 'Pagalan',
+        lastname: 'Zulieta',
+        sex: 'male',
+        email: 'jaypee.zulieta@lsu.edu.ph',
+        password: 'Xscvsdg5417!',
+      };
+      const guardsPipe = new GuardsPipe();
+
+      const guard = guardsPipe.transform(data);
+      expect(guard).resolves.toBeInstanceOf(Guard);
+    });
   });
 });
