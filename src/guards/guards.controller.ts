931e9c985144aee0a25bc4e7654ef66351a7b2d4
JaypsZulieta <jayps.zulieta@gmail.com>
2024-05-17 11:16:23 +0800

    fix! (GuardsController): remove unused imports and test method in GuardsController

--- src/guards/guards.controller.ts
+++ src/guards/guards.controller.ts
@@ -1,13 +1,8 @@
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
-import { GuardsPipe } from './guards.pipe';
-import { Guard } from './guards.entity';
+import {  Controller,  UseFilters } from '@nestjs/common';
 import { ValidationExceptionFilter } from 'src/validation.exception-filter';
 
 @Controller('guards')
 @UseFilters(ValidationExceptionFilter)
 export class GuardsController {
-  @Post()
-  getGuard(@Body(GuardsPipe) guard: Guard): Guard {
-    return guard;
-  }
+  
 }
