6de30d928f5d3765ddd39f0f8065c6c2ad0568bf
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-17 21:38:05 +0800

    feat: Add Swagger Document to the bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Arrivals and Departures Service')
+    .setDescription(
+      'A service that monitors the arrivals and departures of students',
+    )
+    .setVersion('1.0')
+    .addTag('a-and-d')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
