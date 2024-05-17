cea89148f633c87a66e1675f53e9c9ae48fb4468
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-17 20:48:29 +0800

    test (GuardsPipe): Add more tests to the GuardsPipe class

--- src/guards/guards.pipe.spec.ts
+++ src/guards/guards.pipe.spec.ts
@@ -1,5 +1,7 @@
+import { ValidationError } from 'quidquid-picker';
 import { Guard } from './guards.entity';
 import { GuardsPipe } from './guards.pipe';
+import { BadRequestException } from '@nestjs/common';
 
 describe('GuardsPipe', () => {
   it('should be defined', () => {
@@ -20,6 +22,47 @@ describe('GuardsPipe', () => {
 
       const guard = guardsPipe.transform(data);
       expect(guard).resolves.toBeInstanceOf(Guard);
+    });
+
+    it('should throw a ValidationError if the firstname is not of type string', () => {
+      const data = {
+        firstname: 123,
+        middlename: 'Pagalan',
+        lastname: 'Zulieta',
+        sex: 'male',
+        email: 'jaypee.zulieta@lsu.edu.ph',
+        password: 'Xscvsdg5417!',
+      };
+      const guardsPipe = new GuardsPipe();
+      const guard = guardsPipe.transform(data);
+      expect(guard).rejects.toThrow(ValidationError);
+    });
+
+    it('should throw a ValidationError if the firstname is not present in the data', () => {
+      const data = {
+        middlename: 'Pagalan',
+        lastname: 'Zulieta',
+        sex: 'male',
+        email: 'jaypee.zulieta@lsu.edu.ph',
+        password: 'Xscvsdg5417!',
+      };
+      const guardsPipe = new GuardsPipe();
+      const guard = guardsPipe.transform(data);
+      expect(guard).rejects.toThrow(ValidationError);
+    });
+
+    it('should throw a BadRequestException if the sex in the data is not valid', () => {
+      const data = {
+        firstname: 'Jaypee',
+        middlename: 'Pagalan',
+        lastname: 'Zulieta',
+        sex: 'Apache Attack Helicopter',
+        email: 'jaypee.zulieta@lsu.edu.ph',
+        password: 'Xscvsdg5417!',
+      };
+      const guardsPipe = new GuardsPipe();
+      const guard = guardsPipe.transform(data);
+      expect(guard).rejects.toThrow(BadRequestException);
     });
   });
 });
