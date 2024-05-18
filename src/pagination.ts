a779782e64ed1f4cb9f203fc4d80ea427f6deeba
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-18 13:03:36 +0800

    feat: Add paginationOptions

---
+++ src/pagination.ts
@@ -0,0 +1,11 @@
+export type PaginationOptions = {
+  pageNumber: number;
+  pageSize?: number;
+};
+
+export type PaginatedContent<TContent> = {
+  totalItems: number;
+  totalPages: number;
+  content: TContent[];
+  currentPage: number;
+};
