fd7399089d810da0113be27fbc635069822b3e11
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-19 13:06:14 +0800

    feat (Auth): Add Authentication entities

---
+++ src/auth/auth.entity.ts
@@ -0,0 +1,43 @@
+import { UserDetails } from 'src/users/users.service';
+
+export class Authentication {
+  private userDetails: UserDetails;
+  private accessToken: string;
+  private refreshToken: string;
+
+  constructor(userDetails: UserDetails, accessToken: string, refreshToken: string) {
+    this.userDetails = userDetails;
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+
+  getUserDetails(): UserDetails {
+    return this.userDetails;
+  }
+
+  getAccessToken(): string {
+    return this.accessToken;
+  }
+
+  getRefreshToken(): string {
+    return this.refreshToken;
+  }
+}
+
+export class EmailAndPasswordCredentials {
+  private email: string;
+  private password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  getPassword(): string {
+    return this.password;
+  }
+}
