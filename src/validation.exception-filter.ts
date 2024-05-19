237f367ee49c1c79e2b529008f0173a0fdae7503
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-19 15:15:50 +0800

    fix (validationExceptionFilter): remove unused imports

--- src/validation.exception-filter.ts
+++ src/validation.exception-filter.ts
@@ -1,11 +1,8 @@
 import { ArgumentsHost, ExceptionFilter } from '@nestjs/common';
-import { ValidationError } from "quidquid-picker";
+import { ValidationError } from 'quidquid-picker';
 import { Response, Request } from 'express';
-import path from 'path';
 
-export class ValidationExceptionFilter
-  implements ExceptionFilter<ValidationError>
-{
+export class ValidationExceptionFilter implements ExceptionFilter<ValidationError> {
   catch(exception: ValidationError, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const response = context.getResponse<Response>();
@@ -15,7 +12,7 @@ export class ValidationExceptionFilter
       statusCode: 400,
       message: exception.message,
       timeStamp: new Date().toISOString(),
-      path: request.path
+      path: request.path,
     });
   }
 }
