54bad9a642e0a656ce3d7f00e878ab12da315a71
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-19 11:08:06 +0800

    feat (Users): Add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GuardsModule } from './guards/guards.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [GuardsModule, AuthModule],
+  imports: [GuardsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
