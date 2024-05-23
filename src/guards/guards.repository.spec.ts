05e430b7e9190a19abb0129786c4fe662072f344
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-23 21:08:36 +0800

    fix!: refactor test so that they work properly

--- src/guards/guards.repository.spec.ts
+++ src/guards/guards.repository.spec.ts
@@ -3,13 +3,17 @@ import { GuardBuilder } from './guards.entity';
 import { GuardsRepository, PrismaGuardRepository } from './guards.repository';
 
 describe('GuardsRepository', () => {
-  let prismaGuardsRepository: GuardsRepository;
-  let prismaService: PrismaService;
+  const prismaService = new PrismaService();
+  const prismaGuardsRepository = new PrismaGuardRepository(prismaService);
+  const createGuardSpy = jest
+    .spyOn(prismaService.guard, 'create')
+    .mockResolvedValue({} as any);
+  const createPersonSpy = jest
+    .spyOn(prismaService.person, 'create')
+    .mockResolvedValue({} as any);
 
   beforeEach(() => {
     jest.clearAllMocks();
-    prismaService = new PrismaService();
-    prismaGuardsRepository = new PrismaGuardRepository(prismaService);
   });
 
   describe('save', () => {
@@ -17,21 +21,14 @@ describe('GuardsRepository', () => {
       const existByIdSpy = jest
         .spyOn(prismaGuardsRepository, 'existById')
         .mockResolvedValue(false);
-      const createGuardSpy = jest
-        .spyOn(prismaService.guard, 'create')
-        .mockResolvedValue({} as any);
-      const createPersonSpy = jest
-        .spyOn(prismaService.person, 'create')
-        .mockResolvedValue({} as any);
 
       const guard = new GuardBuilder().build();
 
-      prismaGuardsRepository.save(guard).then(() => {
-        // Assert
-        expect(existByIdSpy).toHaveBeenCalledWith(guard.getId());
-        expect(createPersonSpy).toHaveBeenCalledTimes(1);
-        expect(createGuardSpy).toHaveBeenCalledTimes(1);
-      });
+      await prismaGuardsRepository.save(guard);
+
+      expect(existByIdSpy).toHaveBeenCalledWith(guard.getId());
+      expect(createPersonSpy).toHaveBeenCalledTimes(1);
+      expect(createGuardSpy).toHaveBeenCalledTimes(1);
     });
 
     test('should update a record in the person and guard tables if the guard does exist', async () => {
@@ -45,11 +42,14 @@ describe('GuardsRepository', () => {
         .spyOn(prismaService.guard, 'update')
         .mockResolvedValue({} as any);
       const guard = new GuardBuilder().build();
-      prismaGuardsRepository.save(guard).then(() => {
-        expect(existByIdSpy).toHaveBeenCalledWith(guard.getId());
-        expect(updatePersonSpy).toHaveBeenCalledTimes(1);
-        expect(updateGuardSpy).toHaveBeenCalledTimes(1);
-      });
+
+      await prismaGuardsRepository.save(guard);
+
+      expect(updatePersonSpy).toHaveBeenCalledTimes(1);
+      expect(updateGuardSpy).toHaveBeenCalledTimes(1);
+      expect(existByIdSpy).toHaveBeenCalledWith(guard.getId());
+      expect(createGuardSpy).not.toHaveBeenCalled();
+      expect(createPersonSpy).not.toHaveBeenCalled();
     });
   });
 });
