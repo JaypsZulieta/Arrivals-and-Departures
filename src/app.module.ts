cdcb8269716775a8c2ead53f54849c46344be3a5
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-25 01:54:13 +0800

    feat (Tracks): Add tracks module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PasswordModule } from './password/password.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TracksModule } from './tracks/tracks.module';
 
 @Module({
-  imports: [GuardsModule, AuthModule, UsersModule, PasswordModule, PrismaModule],
+  imports: [GuardsModule, AuthModule, UsersModule, PasswordModule, PrismaModule, TracksModule],
 })
 export class AppModule {}
