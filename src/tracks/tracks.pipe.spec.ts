df0c4068b56f3108c2c5f4d0c75f162fed94660d
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-25 15:41:01 +0800

    feat (Track): Add tracksPipe

--- src/tracks/tracks.pipe.spec.ts
+++ src/tracks/tracks.pipe.spec.ts
@@ -1,7 +1,26 @@
+import { ValidationError } from 'quidquid-picker';
 import { TracksPipe } from './tracks.pipe';
+import { Track } from './track.entity';
+
+const tracksPipe = new TracksPipe();
 
 describe('TracksPipe', () => {
   it('should be defined', () => {
     expect(new TracksPipe()).toBeDefined();
+  });
+
+  describe('transform', () => {
+    test('should throw a ValidationError if the data is not acceptable', async () => {
+      const data = {};
+      expect(async () => {
+        await tracksPipe.transform(data);
+      }).rejects.toThrow(ValidationError);
+    });
+    test('should return a instance of Track if the data is acceptable', async () => {
+      const data = { name: 'TVL' };
+      const track = await tracksPipe.transform(data);
+      expect(track).toBeInstanceOf(Track);
+      expect(track.getName()).toBe(data.name);
+    });
   });
 });
--- src/tracks/tracks.pipe.ts
+++ src/tracks/tracks.pipe.ts
@@ -1,8 +1,12 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { Track, TrackBuilder } from './track.entity';
+import { Quidquid } from 'quidquid-picker';
 
 @Injectable()
-export class TracksPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+export class TracksPipe implements PipeTransform<any, Promise<Track>> {
+  public async transform(value: any): Promise<Track> {
+    const data = Quidquid.from(value);
+    const name = await data.pickString('name');
+    return new TrackBuilder().name(name).build();
   }
 }
