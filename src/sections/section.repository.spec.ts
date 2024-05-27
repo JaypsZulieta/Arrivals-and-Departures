aebb3f73221bfac2d396467b252ebf32b7bd87d8
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-27 14:19:22 +0800

    test (SectionRepository): Add test

---
+++ src/sections/section.repository.spec.ts
@@ -0,0 +1,46 @@
+import { PrismaService } from '../prisma/prisma.service';
+import { PrismaSectionRepository } from './section.repository';
+import { SectionBuilder } from './sections.entity';
+
+const prismaService = new PrismaService();
+const repository = new PrismaSectionRepository(prismaService);
+
+const createSpy = jest
+  .spyOn(prismaService.section, 'create')
+  .mockResolvedValue({} as any);
+const updateSpy = jest
+  .spyOn(prismaService.section, 'update')
+  .mockResolvedValue({} as any);
+jest.spyOn(prismaService.track, 'findUniqueOrThrow').mockResolvedValue({
+  id: '123',
+  trackName: 'TVL',
+  creationDate: new Date(),
+});
+jest.spyOn(prismaService.strand, 'findUniqueOrThrow').mockResolvedValue({
+  id: '123',
+  strandName: 'Programming',
+  trackId: '123',
+  creationDate: new Date(),
+});
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('PrismaSectionRepository', () => {
+  describe('save', () => {
+    test('should call the update method if the entry already exists in the database', async () => {
+      jest.spyOn(repository, 'existById').mockResolvedValue(true);
+      const section = new SectionBuilder().build();
+      await repository.save(section);
+      expect(updateSpy).toHaveBeenCalledTimes(1);
+    });
+
+    test('should call the create method if the entry does not exist in the databse', async () => {
+      jest.spyOn(repository, 'existById').mockResolvedValue(false);
+      const section = new SectionBuilder().build();
+      await repository.save(section);
+      expect(createSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
