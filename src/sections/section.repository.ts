d8b7eda97dc8f1fa3fa98c7c822675f17db43dc0
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-27 13:13:51 +0800

    fix (SectionRepository): minor code clean up

--- src/sections/section.repository.ts
+++ src/sections/section.repository.ts
@@ -2,16 +2,14 @@ import { PaginatedContent, PaginationOptions } from '../pagination';
 import { Section } from './sections.entity';
 
 export abstract class SectionRepository {
-  public abstract save(section: Section): Promise<Section>;
-  public abstract findById(id: string): Promise<Section>;
-  public abstract findByName(id: string): Promise<Section>;
-  public abstract findAll(
-    options?: PaginationOptions,
-  ): Promise<PaginatedContent<Section>>;
-  public abstract existById(id: string): Promise<boolean>;
-  public abstract existByName(name: string): Promise<boolean>;
-  public abstract count(): Promise<number>;
-  public abstract delete(section: Section): Promise<Section>;
+  abstract save(section: Section): Promise<Section>;
+  abstract findById(id: string): Promise<Section>;
+  abstract findByName(id: string): Promise<Section>;
+  abstract findAll(options?: PaginationOptions): Promise<PaginatedContent<Section>>;
+  abstract existById(id: string): Promise<boolean>;
+  abstract existByName(name: string): Promise<boolean>;
+  abstract count(): Promise<number>;
+  abstract delete(section: Section): Promise<Section>;
 }
 
 export const mockSectionRepository = {
