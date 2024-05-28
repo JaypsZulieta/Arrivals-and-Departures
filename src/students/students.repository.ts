6e616d54fa69af983556b3b078609738f07a4501
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-28 10:24:00 +0800

    feat! (StudentService): Add StandardStudentService

--- src/students/students.repository.ts
+++ src/students/students.repository.ts
@@ -4,7 +4,7 @@ import { Student } from './student.entity';
 export abstract class StudentRepository {
   abstract save(student: Student): Promise<Student>;
   abstract findByLrn(lrn: string): Promise<Student>;
-  abstract findAll(options: PaginationOptions): Promise<PaginatedContent<Student>>;
+  abstract findAll(options?: PaginationOptions): Promise<PaginatedContent<Student>>;
   abstract existByLrn(lrn: string): Promise<boolean>;
   abstract count(): Promise<number>;
   abstract delete(student: Student): Promise<Student>;
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,12 +1,64 @@
-import { PaginatedContent, PaginationOptions } from 'src/pagination';
+import { PaginatedContent, PaginationOptions } from '../pagination';
 import { Student } from './student.entity';
+import { StudentRepository } from './students.repository';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 
 export abstract class StudentService {
   abstract create(student: Student): Promise<Student>;
   abstract findByLrn(lrn: string): Promise<Student>;
   abstract existByLrn(lrn: string): Promise<boolean>;
   abstract count(): Promise<number>;
-  abstract findAll(options: PaginationOptions): Promise<PaginatedContent<Student>>;
+  abstract findAll(options?: PaginationOptions): Promise<PaginatedContent<Student>>;
   abstract update(student: Student): Promise<Student>;
   abstract delete(lrn: string): Promise<void>;
+}
+
+@Injectable()
+export class StandardStudentService implements StudentService {
+  private repository: StudentRepository;
+
+  constructor(repository: StudentRepository) {
+    this.repository = repository;
+  }
+
+  public async create(student: Student): Promise<Student> {
+    const learnerReferenceNumber = student.getLearnerReferenceNumber();
+    if (!(await this.existByLrn(learnerReferenceNumber))) {
+      const conflictMessage = `student with lrn '${learnerReferenceNumber}' already exists`;
+      throw new ConflictException(conflictMessage);
+    }
+    return await this.repository.save(student);
+  }
+
+  public async findByLrn(lrn: string): Promise<Student> {
+    if (!(await this.existByLrn(lrn)))
+      throw new NotFoundException(`student with lrn '${lrn}' does not exist`);
+    return await this.repository.findByLrn(lrn);
+  }
+
+  public async existByLrn(lrn: string): Promise<boolean> {
+    return await this.repository.existByLrn(lrn);
+  }
+
+  public async count(): Promise<number> {
+    return await this.repository.count();
+  }
+
+  public async findAll(options?: PaginationOptions): Promise<PaginatedContent<Student>> {
+    return await this.repository.findAll(options);
+  }
+
+  public async update(student: Student): Promise<Student> {
+    const learnerReferenceNumber = student.getLearnerReferenceNumber();
+    if (!(await this.existByLrn(learnerReferenceNumber))) {
+      const conflictMessage = `student with lrn '${learnerReferenceNumber}' does not exist`;
+      throw new NotFoundException(conflictMessage);
+    }
+    return await this.repository.save(student);
+  }
+
+  public async delete(lrn: string): Promise<void> {
+    const student = await this.findByLrn(lrn);
+    await this.repository.delete(student);
+  }
 }
