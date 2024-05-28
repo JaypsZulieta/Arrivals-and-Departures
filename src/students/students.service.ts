1f35bdb54cf92015b3675f5643debca6597f4d2f
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-28 09:34:18 +0800

    feat (StudentService): Add StudentService abtract class

---
+++ src/students/students.service.ts
@@ -0,0 +1,12 @@
+import { PaginatedContent, PaginationOptions } from 'src/pagination';
+import { Student } from './student.entity';
+
+export abstract class StudentService {
+  abstract create(student: Student): Promise<Student>;
+  abstract findByLrn(lrn: string): Promise<Student>;
+  abstract existByLrn(lrn: string): Promise<boolean>;
+  abstract count(): Promise<number>;
+  abstract findAll(options: PaginationOptions): Promise<PaginatedContent<Student>>;
+  abstract update(student: Student): Promise<Student>;
+  abstract delete(id: string): Promise<void>;
+}
