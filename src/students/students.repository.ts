d332b6060221c074af0b79b2f0c7a26094acb3bf
sexy-turtle5417 <jaypee.zulieta@lsu.edu.ph>
2024-05-28 09:33:53 +0800

    fix (StudentRepository): fix the return types

--- src/students/students.repository.ts
+++ src/students/students.repository.ts
@@ -5,7 +5,7 @@ export abstract class StudentRepository {
   abstract save(student: Student): Promise<Student>;
   abstract findByLrn(lrn: string): Promise<Student>;
   abstract findAll(options: PaginationOptions): Promise<PaginatedContent<Student>>;
-  abstract existByLrn(lrn: string): Promise<Student>;
-  abstract count(): Promise<Student>;
+  abstract existByLrn(lrn: string): Promise<boolean>;
+  abstract count(): Promise<number>;
   abstract delete(student: Student): Promise<Student>;
 }
